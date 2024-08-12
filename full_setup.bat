345392430fa397db57cceb2fd859486ead56e920
neurokitti <168581144+neurokitti@users.noreply.github.com>
2024-08-12 02:56:28 -0500

    Update full_setup.bat

--- full_setup.bat
+++ full_setup.bat
@@ -15,10 +15,5 @@ pip install -r Arc_API\requirements-arc-api.txt
 echo.
 echo Installation complete! 
 echo.
-echo For best performance, please adjust your Windows settings:
-echo - Wallpaper Settings: Set to "Stretch"
-echo - Arc Theme: Set to "Mica"
-echo - Theme: Set to "Transparent image"
-echo.
 
 pause

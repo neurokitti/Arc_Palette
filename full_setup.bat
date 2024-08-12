a866516858ed6ea9ef82371b14a64f95e1ab111d
neurokitti <168581144+neurokitti@users.noreply.github.com>
2024-08-12 02:55:25 -0500

    Create full_setup.bat

---
+++ full_setup.bat
@@ -0,0 +1,24 @@
+@echo off
+echo Cloning Arc Palette Repo...
+git clone https://github.com/neurokitti/Arc_Palette.git
+cd Arc_Palette
+echo Cloning Arc_API...
+git clone https://github.com/neurokitti/Arc_API.git
+echo creating venv... 
+python -m venv .venv
+echo activating venv... 
+call .venv\Scripts\activate
+echo installing requirments... 
+pip install -r requirements.txt
+pip install -r Arc_API\requirements-arc-api.txt
+
+echo.
+echo Installation complete! 
+echo.
+echo For best performance, please adjust your Windows settings:
+echo - Wallpaper Settings: Set to "Stretch"
+echo - Arc Theme: Set to "Mica"
+echo - Theme: Set to "Transparent image"
+echo.
+
+pause

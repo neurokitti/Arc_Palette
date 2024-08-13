7aa103e6d0ff1630afd17aadcbc2e93e1df45f85
neurokitti <168581144+neurokitti@users.noreply.github.com>
2024-08-12 22:31:38 -0500

    Update full_setup.bat

--- full_setup.bat
+++ full_setup.bat
@@ -1,14 +1,39 @@
 @echo off
-echo Cloning Arc Palette Repo...
-git clone https://github.com/neurokitti/Arc_Palette.git
+
+:: Check if Python is installed
+python --version >nul 2>&1
+IF ERRORLEVEL 1 (
+    echo Python is not installed. Please install Python 3.6 or higher and try again.
+    pause
+    exit /b
+)
+
+echo Downloading Arc Palette Repo...
+curl -L https://github.com/neurokitti/Arc_Palette/archive/refs/heads/main.zip -o Arc_Palette.zip
+echo Unzipping Arc Palette...
+tar -xf Arc_Palette.zip
+rename Arc_Palette-main Arc_Palette
+echo Deleting Arc Palette ZIP file...
+del Arc_Palette.zip
+rd /s /q Arc_Palette-main
+
 cd Arc_Palette
-echo Cloning Arc_API...
-git clone https://github.com/neurokitti/Arc_API.git
-echo creating venv... 
+
+echo Downloading Arc_API...
+curl -L https://github.com/neurokitti/Arc_API/archive/refs/heads/main.zip -o Arc_API.zip
+echo Unzipping Arc_API...
+tar -xf Arc_API.zip
+rename Arc_API-main Arc_API
+echo Deleting Arc_API ZIP file...
+del Arc_API.zip
+rd /s /q Arc_API-main
+
+echo Creating venv... 
 python -m venv .venv
-echo activating venv... 
+echo Activating venv... 
 call .venv\Scripts\activate
-echo installing requirments... 
+
+echo Installing requirements... 
 pip install -r requirements.txt
 pip install -r Arc_API\requirements-arc-api.txt
 
